f216497a9a0c13f977b693a7293700e86070722a
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-02-20 16:43:28 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random  =Math.ceil(Math.random() * 100);
+
+let num3=10;
+let num4=6;
+let mod= num3%num4
+
+
+
+let numbers = [10, 5, 20, 15];
+
+// Find the highest number in the set
+let max = Math.max(...numbers);
